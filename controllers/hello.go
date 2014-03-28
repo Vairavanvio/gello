b8b8bde2249bc29c9466bdc3480758391b0f748f
Scott W. Bradley <scottwb@scottwb.net>
2014-03-27 19:34:17 -0700

    Figure out how to use template files

--- controllers/hello.go
+++ controllers/hello.go
@@ -48,26 +48,7 @@ func root(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-var guestbookTemplate = template.Must(template.New("book").Parse(guestbookTemplateHTML))
-
-const guestbookTemplateHTML = `
-<html>
-  <body>
-	{{range .}}
-	  {{with .Author}}
-		<p><b>{{.}}</b> wrote:</p>
-	  {{else}}
-		<p>An anonymous person wrote:</p>
-	  {{end}}
-	  <pre>{{.Content}}</pre>
-	{{end}}
-	<form action="/sign" method="post">
-	  <div><textarea name="content" rows="3" cols="60"></textarea></div>
-	  <div><input type="submit" value="Sign Guestbook"></div>
-	</form>
-  </body>
-</html>
-`
+var guestbookTemplate = template.Must(template.New("guestbook.html").ParseFiles("views/guestbook.html"))
 
 func sign(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
